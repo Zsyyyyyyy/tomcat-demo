39f8a19c6cbf813dacfdbf68b8c004f257d59d36
zsy <1541101353@qq.com>
2022-07-07 00:15:52 +0800

    Initial commit

---
+++ src/main/java/com/zsy/app/Demo1.java
@@ -0,0 +1,27 @@
+package com.zsy.app;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/req")
+public class Demo1 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("get....");
+
+        String account = req.getParameter("account");
+        String password = req.getParameter("password");
+
+        System.out.println(account+ "_" + password);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req,resp);
+    }
+}
